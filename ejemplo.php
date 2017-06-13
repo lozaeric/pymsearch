53951cf66267f0c24a1ef198a50e2635413a3771
Eric <lozaeric@gmail.com>
2017-06-13 12:47:24 -0300

    Instanciación  agregada

--- ejemplo.php
+++ ejemplo.php
@@ -4,6 +4,10 @@
 	
 	$palabras_clave = ["tablet", "drone", "smartwatch", "camara anti golpes", "gps"];
 	$buscador = new Buscador($palabras_clave);
+	/*
+	** asignando valores a los ratios
+	** $buscador = new Buscador($palabras_clave, 0.5, 3);
+	*/
 	$palabra = "gps";
 	print_r ($buscador->obtener_palabra_clave($palabra));
 
